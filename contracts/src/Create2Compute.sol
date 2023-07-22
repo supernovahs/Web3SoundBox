9a1831e026dbb82879de4f333a3c6a4f6eb5f7be
supernovahs <harshitsinghal252@gmail.com>
2023-07-23 00:20:45 +0200

    compute create 2

--- contracts/src/Create2Compute.sol
+++ contracts/src/Create2Compute.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.19;
 import "openzeppelin/utils/Create2.sol";
-
+import "./SoundBox.sol";
 contract Create2Compute {
-    function getCreate2Address(uint _salt) public returns (address) {
+
+    function getCreate2Address(uint _salt) public view  returns (address) {
         bytes memory bytecode = type(SoundBox).creationCode;
         address ret = Create2.computeAddress(
             bytes32(_salt),
